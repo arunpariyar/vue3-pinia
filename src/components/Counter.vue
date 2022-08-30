95a7245e5ee8f0c5b9b34658f62cff565e9049a6
Arun Pariyar <arun.pariyar@gmail.com>
2022-08-30 12:45:34 +0200

    :sparkles: Pinia configured
    Currently making use map store, however need to look into documentation oncemore

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>Counter Component</h2>
+  <button @click.prevent="addCounter">Add</button>
+  <h2>{{ this.counterStore.counter }}</h2>
+  <button @click.prevetn="subCounter">Substract</button>
+</template>
+<script>
+import { mapStores, mapState, mapWritableState } from "pinia";
+import { useCounterStore } from "@/stores/counter";
+
+export default {
+  name: "Counter",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapStores(useCounterStore, ["counter"]),
+  },
+  methods: {
+    addCounter() {
+      this.counterStore.counter++;
+    },
+    subCounter() {
+      this.counterStore.counter--;
+    },
+  },
+};
+</script>
